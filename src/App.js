155c651d9f256d304495645200455fb2aac6d47c
Mia-KR <szlouolsz@gmail.com>
2024-03-21 17:57:43 +0900

    add alert

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import { useState } from "react";
 import "./App.css";
 import ExpenseForm from "./components/ExpenseForm";
 import ExpenseList from "./components/ExpenseList";
+import Alert from "./components/Alert";
 
-function App() {
+const App = () => {
   const [expenses, setExpenses] = useState([
     { id: 1, charge: "렌트비", amount: 1500 },
     { id: 2, charge: "교통비", amount: 3000 },
@@ -14,7 +15,9 @@ function App() {
 
   const [id, SetId] = useState("");
 
-  const [isEditing, setIsEditing] = useState(false);
+  const [edit, setEdit] = useState(false);
+
+  const [alert, setAlert] = useState({ show: false });
 
   const handleEdit = (id) => {
     const expense = expenses.find((expense) => expense.id === id);
@@ -22,7 +25,12 @@ function App() {
     setCharge(charge);
     setAmount(amount);
     SetId(id);
-    setIsEditing(true);
+    setEdit(true);
+  };
+
+  const handleAlert = ({ type, text }) => {
+    setAlert({ show: true, type, text });
+    setTimeout(() => setAlert({ show: false }), 7000);
   };
 
   const handleDelete = (id) => {
@@ -31,6 +39,7 @@ function App() {
     const newExpenses = expenses.filter((expense) => expense.id !== id);
     console.log(newExpenses);
     setExpenses(newExpenses);
+    handleAlert({ type: "danger", text: "아이템이 삭제되었습니다." });
   };
 
   const handleCharge = (e) => {
@@ -46,12 +55,13 @@ function App() {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (charge !== "" && amount > 0) {
-      if (isEditing) {
+      if (edit) {
         const newExpenses = expenses.map((item) => {
           return item.id === id ? { ...item, charge, amount } : item;
         });
         setExpenses(newExpenses);
-        setIsEditing(false);
+        setEdit(false);
+        handleAlert({ type: "success", text: "아이템이 수정되었습니다." });
       } else {
         const newExpense = {
           id: crypto.randomUUID(),
@@ -60,12 +70,17 @@ function App() {
         };
         const newExpenses = [...expenses, newExpense];
         setExpenses(newExpenses);
+        handleAlert({ type: "success", text: "아이템이 생성되었습니다." });
       }
 
       setCharge("");
       setAmount(0);
     } else {
-      alert("");
+      console.log("error");
+      handleAlert({
+        type: "danger",
+        text: "charge는 빈 값일 수 없으며 amount는 0보다 커야 합니다.",
+      });
     }
   };
 
@@ -75,6 +90,7 @@ function App() {
 
   return (
     <main className="main-container">
+      {alert.show ? <Alert type={alert.type} text={alert.text} /> : null}
       <h1>예산 계산기</h1>
 
       <div
@@ -85,7 +101,7 @@ function App() {
         }}
       >
         <ExpenseForm
-          isEditing={isEditing}
+          edit={edit}
           handleSubmit={handleSubmit}
           handleAmount={handleAmount}
           charge={charge}
@@ -103,7 +119,9 @@ function App() {
         />
       </div>
 
-      <div style={{ displa: "flex", justifyContent: "end", marginTop: "1rem" }}>
+      <div
+        style={{ display: "flex", justifyContent: "end", marginTop: "1rem" }}
+      >
         <p style={{ fontSize: "1.5rem", textAlign: "right" }}>
           총 지출 :
           <span>
@@ -116,6 +134,6 @@ function App() {
       </div>
     </main>
   );
-}
+};
 
 export default App;
